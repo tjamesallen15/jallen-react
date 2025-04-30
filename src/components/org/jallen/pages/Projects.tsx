46236e36486010bedee6b3ee2b63bb8566e64c1d
James Allen <34049950+tjamesallen15@users.noreply.github.com>
2025-04-30 18:07:24 +0800

    Update Projects.tsx

--- src/components/org/jallen/pages/Projects.tsx
+++ src/components/org/jallen/pages/Projects.tsx
@@ -37,56 +37,58 @@ const Projects = () => {
 
           <div className='min-h-[70vh] w-full'>
             <TabsContent value='personal'>
-              <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ 
-                  opacity: 1,
-                  transition: { 
-                    delay: 1.0,
-                    duration: 0.4,
-                    ease: 'easeIn'
-                  }
-                }}
-                className='grid grid-cols md:grid-cols-2 gap-[60px]'
-              >
-                {
-                  applications.map((item, index) => {
-                    return (
-                      <div className='flex-1 flex flex-col justify-center gap-2' key={index}>
-                        <div className='w-full flex justify-between items-center'>
-                          <div className='text-3xl font-extrabold'>{item.title}</div>
-                          <div className='flex flex-row gap-2'>
-                            {
-                              item.site !== null ? 
-                                <a
-                                  href={item.site}
-                                  className='w-[32px] h-[32px] rounded-full bg-white transition-all duration-500 flex justify-center items-center hover:text-primary hover:bg-accent hover:-rotate-360'
-                                >
-                                  <FaGlobe className='text-xl w-[28px] h-[28px]' />
-                                </a>
-                              : null
-                            }
-                            {
-                              item.repository !== 'N/A' ?
-                                <a 
-                                  href={item.repository}  
-                                  className='w-[32px] h-[32px] rounded-full bg-white transition-all duration-500 flex justify-center items-center hover:text-primary hover:bg-accent hover:-rotate-360'
-                                >
-                                  <FaGithub className='text-xl w-[28px] h-[28px]'/>
-                                </a>
-                              : null
-                            }
+              <ScrollArea className='h-[525px]'>
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ 
+                    opacity: 1,
+                    transition: { 
+                      delay: 1.0,
+                      duration: 0.4,
+                      ease: 'easeIn'
+                    }
+                  }}
+                  className='grid grid-cols gap-[60px]'
+                >
+                  {
+                    applications.map((item, index) => {
+                      return (
+                        <div className='flex-1 flex flex-col justify-center gap-2' key={index}>
+                          <div className='w-full flex justify-between items-center'>
+                            <div className='text-3xl font-extrabold'>{item.title}</div>
+                            <div className='flex flex-row gap-2 pe-4'>
+                              {
+                                item.site !== null ? 
+                                  <a
+                                    href={item.site}
+                                    className='w-[32px] h-[32px] rounded-full bg-white transition-all duration-500 flex justify-center items-center hover:text-primary hover:bg-accent hover:-rotate-360'
+                                  >
+                                    <FaGlobe className='text-xl w-[28px] h-[28px]' />
+                                  </a>
+                                : null
+                              }
+                              {
+                                item.repository !== 'N/A' ?
+                                  <a 
+                                    href={item.repository}  
+                                    className='w-[32px] h-[32px] rounded-full bg-white transition-all duration-500 flex justify-center items-center hover:text-primary hover:bg-accent hover:-rotate-360'
+                                  >
+                                    <FaGithub className='text-xl w-[28px] h-[28px]'/>
+                                  </a>
+                                : null
+                              }
+                            </div>
                           </div>
+                          <h2 className='text-[18px] font-bold leading-none text-accent transition-all duration-500'>{item.tech}</h2>
+                          <h2 className='text-[14px] text-foreground font-semibold leading-none'>{item.category}</h2>
+                          <p className='text-foreground mt-4 font-karla'>{item.description}</p>
+                          <div className='border-b border-sidebar w-full' />
                         </div>
-                        <h2 className='text-[18px] font-bold leading-none text-accent transition-all duration-500'>{item.tech}</h2>
-                        <h2 className='text-[14px] text-foreground font-semibold leading-none'>{item.category}</h2>
-                        <p className='text-foreground mt-4 font-karla'>{item.description}</p>
-                        <div className='border-b border-sidebar w-full' />
-                      </div>
-                    )
-                  })
-                }
-              </motion.div>
+                      )
+                    })
+                  }
+                </motion.div>
+              </ScrollArea>
             </TabsContent>
             <TabsContent value='work'>
               <ScrollArea className='h-[525px]'>
